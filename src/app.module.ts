99210562d4d5af1b88cb480ef4f6316f36be6fc5
yashkataraei <yashkataraei@gmail.com>
2025-08-26 12:42:42 +0530

    yes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [TweetModule],
+  imports: [TweetModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    entities: [],
+    synchronize: true,
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'India@#2047',
+    database: 'nestjs',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
